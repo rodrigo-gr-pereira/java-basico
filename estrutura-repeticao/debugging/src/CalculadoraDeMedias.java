fcda987aafd594af135bc46920498461adba9c11
Rodrigo Pereira <rodrigogr.pereira@gmail.com>
2024-08-08 19:57:18 -0300

    Debugging

---
+++ estrutura-repeticao/debugging/src/CalculadoraDeMedias.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CalculadoraDeMedias {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] alunos = {"Camila", "Rodrigo","Leid", "Alana"};
+
+        int media = calculaMediaDaTurma(alunos, scan);
+
+
+        System.out.printf( "Media da turma %d", media);
+
+
+    }
+
+    public static int calculaMediaDaTurma (String[] alunos, Scanner scanner){
+
+        int soma = 0;
+        for(String aluno : alunos){
+            System.out.printf("Nota do aluno %s: ",aluno);
+            int nota = scanner.nextInt();
+            soma += nota;
+        }
+
+        return soma /alunos.length;
+
+    }
+}
